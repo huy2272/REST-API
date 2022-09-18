3f18f93d8dfabd82be3c0de23c23b4d308ee1660
huy227 <huy2272@gmail.com>
2022-09-18 19:44:23 -0400

    Added a controller file
    VS Studio crashes every time a new controller file is added.
    Right click the newly created controller file to 'include to project'

---
+++ projectAPI/Controllers/QuotesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using projectAPI.Models;
+
+namespace projectAPI.Controllers
+{
+    public class QuotesController : ApiController
+    {
+        static List<Quote> _quotes = new List<Quote>()
+        {
+            new Quote()
+            {
+                Id = 0, Author = "Einstein", Description = "Imagination is more important than knowledge",
+                Title = "Imagination"
+            },
+            new Quote()
+            {
+                Id = 1, Author = "Einstein", Description = "Imagination is more important than knowledge",
+                Title = "Imagination"
+            },
+        };
+
+        public IEnumerable<Quote> Get()
+        {
+            return _quotes;
+        }
+
+        public void Post(Quote quote)
+        {
+            _quotes.Add(quote);
+        }
+    }
+}
